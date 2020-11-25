4cb3b853791e1de6dc5f224106328963217fcc40
Breitholtz <cadbreitholtz@protonmail.com>
2020-11-25 17:15:43 +0100

    initial commit

---
+++ chalmersfood.sh
@@ -0,0 +1,9 @@
+#http://cafelinsen.se/menyer/cafe-linsen-lunch-meny.pdf
+
+#https://chalmerskonferens.se/en/lunchmenyer-lindholmen/
+
+#https://wijkanders.se/en/restaurangen/
+
+#http://restaurang-einstein.se/
+
+#https://chalmerskonferens.se/en/lunchmenyer-johanneberg/
